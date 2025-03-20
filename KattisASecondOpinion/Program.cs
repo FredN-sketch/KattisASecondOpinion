17c32d3aef99a8101f113d9dc835f07b801c45cd
admin <admin@DESKTOP-NIER39A>
2025-03-20 11:09:44 +0100

    Add project files.

---
+++ KattisASecondOpinion/Program.cs
@@ -0,0 +1,27 @@
+﻿ // https://open.kattis.com/problems/secondopinion
+
+namespace KattisASecondOpinion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of seconds: ");
+            try
+            {
+                int input = int.Parse(Console.ReadLine());
+                int hours = input / (60 * 60);
+                int minutes = (input / 60) % 60;
+                int seconds = input % 60;
+                Console.WriteLine();
+                Console.WriteLine("{0}:{1}:{2}", hours, minutes, seconds);
+            }
+            catch 
+            {
+                Console.WriteLine("Input is invalid");             
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
